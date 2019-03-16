8d8911c6179e953f0d1ef0504e78205ca6f7356a
rafeefnajeh <38864039+rafeefnajeh@users.noreply.github.com>
2019-03-16 19:40:48 +0200

    Add files via upload

---
+++ js/min.js
@@ -0,0 +1,30 @@
+/*  global $ , alert,console, click */
+
+$(function(){
+
+//  Adjust slider height
+
+    var winH = $(window).height(),
+        upperH = $('.upper-up').innerHeight(),
+        navH = $('.navbar').innerHeight();
+
+    $('.slider , .carousel-item').height(winH - (upperH + navH));
+
+
+//    featured work shuffle
+
+    $('.featured-work ul li').on('click' , function(){
+
+      $(this).addClass('active').siblings().removeClass('active');
+
+       if($(this).data('class') === 'all'){
+           $('.shuffle-images .col-sm').css('opacity' , 1);
+       }else{
+           $('.shuffle-images .col-sm').css('opacity' , '.08');
+           $($(this).data('class')).parent().css('opacity' , 1);
+       }
+
+    });
+
+
+});
